9f6fe0e116e547dbd115239ab5611d2b540eec94
KipronohVincent <kipronohvincent43@gmail.com>
2022-11-20 11:41:45 +0300

    user data

---
+++ initialData.js
@@ -0,0 +1,13 @@
+const users = [
+    {
+      _id: 1587912,
+      api_key: "rwuy6434tgdgjhtiojiosi838tjue3",
+      username: "username",
+      usage: [{ date: "2022-10-10", count: 17 }],
+    },
+  ];
+  const Countries = [
+    { _id: 1, name: "Kenya" },
+    { _id: 2, name: "SA" },
+  ];
+  module.exports = { users, Countries };
